401729106a1851f79bf8eea5d6722e762422a6a7
gloureirogomes <loureirobiel@gmail.com>
2024-03-18 21:06:01 -0300

    first commit

---
+++ src/test/java/br/com/estudos/bookmanagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
